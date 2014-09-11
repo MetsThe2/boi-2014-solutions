a051009163be9a301bcbb5c7d887df8d58a6267d
MetsThe2 <MetsThe2@users.noreply.github.com>
2014-09-11 09:44:14 +0300

    Fixed leak

--- coprobber.c
+++ coprobber.c
@@ -3,10 +3,10 @@
 #include <stdlib.h>
 #include <assert.h>
 
-unsigned middle_of(bool **A, unsigned N)
+static unsigned middle_of(bool **A, unsigned N)
 {
-	// Minimize maximum distance from any intersection.
-	return N / 2; // Assuming the intersections are numbered in order.
+  // Minimize maximum distance from any intersection.
+  return N / 2; // Assuming the intersections are numbered in order.
 }
 
 static bool **connected;
@@ -15,32 +15,34 @@ static unsigned cop;
 
 int start(unsigned N, bool **A)
 {
-	const size_t cnct_size = N * N * sizeof(bool);
-	connected = malloc(cnct_size);
-	assert(connected);
-	memcpy(connected, A, cnct_size);
-    
-	intersect_num = N;
-    
-	return max_cycle_len(A, N) > 3 ? -1 : cop = middle_of(A, N);
+  if (max_cycle_len(A, N) > 3) return -1;
+
+  const size_t cnct_size = N * N * sizeof(bool);
+  connected = malloc(cnct_size);
+  assert(connected);
+  memcpy(connected, A, cnct_size);
+
+  intersect_num = N;
+
+  return cop = middle_of(A, N);
 }
 
-void move_cop(unsigned robber, unsigned target_dist)
+static void move_cop(unsigned robber, unsigned target_dist)
 {
-	// cop = something
-	assert(cop_robber_dist(robber) == target_dist);
+  // cop = something
+  assert(cop_robber_dist(robber) == target_dist);
 }
 
 unsigned nextMove(unsigned R)
 {
-	const unsigned d = cop_robber_dist(R);
-	if (d) {
-    	move_cop(R, d - 1);
-	} else if (connected) {
-    	free(connected);
-    	connected = NULL;
-	}
-	return cop;
+  const unsigned d = cop_robber_dist(R);
+  if (d) {
+    move_cop(R, d - 1);
+  } else if (connected) {
+    free(connected);
+    connected = NULL;
+  }
+  return cop;
 }
 
 int main(void)
@@ -50,14 +52,14 @@ int main(void)
   unsigned N = getN();
   bool **A = getA();
   
-	int C = start(N, A);
-	if (C == -1) return 1;
+  int C = start(N, A);
+  if (C == -1) return 1;
+
+  unsigned R;
+  do {
+    R = getR(N, A, C);
+    C = next_move(R);
+  } while (C != R);
     
-	unsigned R;
-	do {
-    	R = getR(N, A, C);
-    	C = next_move(R);
-	} while (C != R);
-    
-	return 0;
+  return 0;
 }
